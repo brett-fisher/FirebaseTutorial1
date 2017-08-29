b545f63c992d8bfe5eb6bba4925aeb772035a7b3
Brett Fisher <bdf5106@psu.edu>
2017-08-29 10:15:19 -0400

    UI complete along with relevant view controller files added.

---
+++ FirebaseTuturial1/SignUpViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SignUpViewController.swift
+//  FirebaseTuturial1
+//
+//  Created by Brett Fisher on 8/29/17.
+//  Copyright © 2017 Fish. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    
+}
