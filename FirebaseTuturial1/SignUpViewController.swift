71d9b16026371b39382e2443318f1eed9cc781ed
Brett Fisher <bdf5106@psu.edu>
2017-08-29 12:06:52 -0400

    Create user functionality added

--- FirebaseTuturial1/SignUpViewController.swift
+++ FirebaseTuturial1/SignUpViewController.swift
@@ -7,8 +7,36 @@
 //
 
 import UIKit
+import Firebase
 
 class SignUpViewController: UIViewController {
-
     
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func createAccountAction(_ sender: Any) {
+        
+        if emailTextField.text == "" {
+            let alert = UIAlertController(title: "Error", message: "Please enter your email and password", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alert.addAction(action)
+            
+            present(alert, animated: true, completion: nil)
+        } else {
+            Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+                
+                if error == nil {
+                    print("Sign up successful")
+                    let viewController = self.storyboard?.instantiateViewController(withIdentifier: "LogIn")
+                    self.present(viewController!, animated: true, completion: nil)
+                } else {
+                    let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    let action = UIAlertAction(title: "Ok", style: .cancel , handler: nil)
+                    alert.addAction(action)
+                    
+                    self.present(alert, animated: true, completion: nil)
+                }
+            }
+        }
+    }
 }
